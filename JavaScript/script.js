05f1af160ac00276503e77caf08a1137182e9dc9
Pablo Esquilache <pabloesquilache@gmail.com>
2022-07-11 00:15:58 -0300

    va queriendo

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,8 @@
 const seccion_alamcen = [
-    {id: 1, nombre: "Arroz" , precio: 69, stock: 100},
-    {id: 2, nombre: "Azucar", precio: 82, stock: 100},
-    {id: 3, nombre: "Yerba", precio: 350, stock: 100},
-    {id: 4, nombre: "Aceite", precio: 420, stock: 100}
+    {id: 1, nombre: "Arroz" , img: "../imagenes/producto-almacen-arroz.jpg",  precio: 69, stock: 100},
+    {id: 2, nombre: "Azucar", img: "../imagenes/producto-almacen-azucar.jpg", precio: 82, stock: 100},
+    {id: 3, nombre: "Yerba", img: "../imagenes/producto-almacen-yerba.jpg", precio: 350, stock: 100},
+    {id: 4, nombre: "Aceite", img: "../imagenes/producto-almacen-aceite.jpg", precio: 420, stock: 100}
 ]
 const seccion_limpieza = [
     {id: 1, nombre: "Lavandina" , precio: 69, stock: 100},
@@ -31,10 +31,19 @@ const seccion_perfumeria = [
 
 const div_almacen = document.getElementById('seccion_almacen')
 
-div_almacen.innerHTML = " "
+seccion_alamcen.forEach(productos => {
+    const pantalla = document.createElement('div');
+    pantalla.innerHTML = `
+        <h2>${productos.nombre}</h2>
+        <img src="${productos.img}" alt="">
+        <p>${productos.precio}</p>
+        <input type="button" value="Añadir al Carrito">
+    `
+    div_almacen.appendChild(pantalla);
+})
 
-for (const producto of seccion_alamcen) {
-    let pantalla = document.createElement("div");
-    pantalla.innerHTML =   `<h2>${producto.nombre}</h2>
-                            <p>${producto.precio}</p>`
-    div_almacen.appendChild(pantalla);}
+// for (const producto of seccion_alamcen) {
+//     let pantalla = document.createElement("div");
+//     pantalla.innerHTML =   `<h2>${producto.nombre}</h2>
+//                             <p>${producto.precio}</p>`
+//     div_almacen.appendChild(pantalla);}
