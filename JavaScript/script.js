4f346ba59825b07314ada3a09ca70c8306efb983
Pablo Esquilache <pabloesquilache@gmail.com>
2022-07-14 09:25:24 -0300

    orden

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,33 +1,4 @@
-const seccion_almacen = [
-    {id: 1, nombre: "Arroz" , img: "../imagenes/producto-almacen-arroz.jpg",  precio: 69, stock: 100},
-    {id: 2, nombre: "Azucar", img: "../imagenes/producto-almacen-azucar.jpg", precio: 82, stock: 100},
-    {id: 3, nombre: "Yerba", img: "../imagenes/producto-almacen-yerba.jpg", precio: 350, stock: 100},
-    {id: 4, nombre: "Aceite", img: "../imagenes/producto-almacen-aceite.jpg", precio: 420, stock: 100}
-]
-const seccion_limpieza = [
-    {id: 1, nombre: "Lavandina" , img: "../imagenes/prducto-limpieza-lavandina.jpg", precio: 69, stock: 100},
-    {id: 2, nombre: "Desodorante", img: "../imagenes/producto-limpieza-desodorante.jpg", precio: 82, stock: 100},
-    {id: 3, nombre: "Detergente", img: "../imagenes/producto-limpieza-detergente.jpg", precio: 350, stock: 100},
-    {id: 4, nombre: "Lisoform", img: "../imagenes/producto-limpieza-lisoform.jpg", precio: 420, stock: 100}
-]
-const seccion_bebida = [
-    {id: 1, nombre: "Vino" , img: "../imagenes/producto-bebidas-vino.jpg", precio: 69, stock: 100},
-    {id: 2, nombre: "Agua", img: "../imagenes/producto-bebidas-agua.jpg", precio: 82, stock: 100},
-    {id: 3, nombre: "Gaseosa", img: "../imagenes/producto-bebidas-gaseosa.jpg", precio: 350, stock: 100},
-    {id: 4, nombre: "Agua Saborizada", img: "../imagenes/producto-bebidas-levite.jpg", precio: 420, stock: 100}
-]
-const seccion_bazar = [
-    {id: 1, nombre: "Cuchillo" , img: "../imagenes/producto-bazar-cuchillo.jpg", precio: 69, stock: 100},
-    {id: 2, nombre: "Tenedor", img: "../imagenes/producto-bazar-tenedor.jpg", precio: 82, stock: 100},
-    {id: 3, nombre: "Cuchara", img: "../imagenes/producto-bazar-cuchara.jpg", precio: 350, stock: 100},
-    {id: 4, nombre: "Olla", img: "../imagenes/producto-bazar-olla.jpg", precio: 420, stock: 100}
-]
-const seccion_perfumeria = [
-    {id: 1, nombre: "Desodorante", img: "../imagenes/producto-perfumeria-desodorante.jpg", precio: 69, stock: 100},
-    {id: 2, nombre: "Perfume", img: "../imagenes/producto-perfumeria-perfumeria.jpg", precio: 82, stock: 100},
-    {id: 3, nombre: "Crema de Manos", img: "../imagenes/producto-perfumeria-crema.jpg", precio: 350, stock: 100},
-    {id: 4, nombre: "Espuma Afeitar", img: "../imagenes/producto-perfumeria-espuma.jpg", precio: 420, stock: 100}
-]
+import {seccion_almacen, seccion_limpieza, seccion_bebida, seccion_bazar, seccion_perfumeria} from './data'
 //-----------------------------------------------------------------------------------------------------------
 const div_almacen = document.getElementById('seccion_almacen')
 
@@ -112,6 +83,7 @@ class Usuario {
         this.passw = passw
     }
 }
+//------------------------------------------------------------------------------------------------------------------------
 
 const formulario_registro = document.getElementById('formulario_registro')
 formulario_registro.addEventListener('submit', (e) => {
@@ -126,8 +98,6 @@ formulario_registro.addEventListener('submit', (e) => {
      const nuevo_usuario = new Usuario(nombre, apellido, sexo, email, usuario, passw)
 
      registro_usuarios.push(nuevo_usuario)
-
-     console.log(registro_usuarios)
 
      formulario_registro.reset()
 })
