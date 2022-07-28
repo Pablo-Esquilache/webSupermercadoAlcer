33172fbccef1d3fe293a42aab26d81374aa1086b
Pablo Esquilache <pabloesquilache@gmail.com>
2022-07-27 23:27:50 -0300

    acomodando

---
+++ JavaScript/carrito.js
@@ -0,0 +1,16 @@
+// import {seccion_almacen, seccion_limpieza, seccion_bebida, seccion_bazar, seccion_perfumeria} from './data.js'
+// //------------------------------------------------------------------------------------------------------------
+// const btn_añadir = document.getElementById('añadir')
+
+// const seccion_carrito = document.getElementById('seccion_carrito')
+
+// btn_añadir.addEventListener('click', ()=> {
+//     seccion_almacen.forEach((producto) => {        
+//         seccion_carrito.innerHTML +=   `<tr>
+//                                             <td><b>${producto}</b></td>
+//                                             <td><b>Cantidad</b></td>
+//                                             <td><b>Precio</b></td>
+//                                             <td><b>Sub Total</b></td>
+//                                         </tr>`
+//     })
+// })
--- JavaScript/productos.js
+++ JavaScript/productos.js
@@ -3,88 +3,58 @@ import {seccion_almacen, seccion_limpieza, seccion_bebida, seccion_bazar, seccio
 //-----------------------------------------------------------------------------------------------------------
 const div_almacen = document.getElementById('seccion_almacen')
 
-seccion_almacen.forEach(productos => {
-    const pantalla = document.createElement('div');
-    pantalla.classList.add('seccionArticuloUnitario')
-    pantalla.innerHTML = `
-        <h2>${productos.nombre}</h2>
-        <img src="${productos.img}" alt="">
-        <p>${productos.precio}</p>
-        <input type="button" id="añadir" value="Añadir al Carrito">
-    `
-    div_almacen.appendChild(pantalla);
+seccion_almacen.forEach((productos, indice) => {
+    div_almacen.innerHTML += `<div  class="seccionArticuloUnitario" id="producto${indice}">
+                            <h2>${productos.nombre}</h2>
+                            <img src="${productos.img}" alt="">
+                            <p>${productos.precio}</p>
+                            <input type="button" id="añadir" value="Añadir al Carrito">
+                        </div>`
 })
 
 const div_lipieza = document.getElementById('seccion_limpieza')
 
-seccion_limpieza.forEach(productos => {
-    const pantalla = document.createElement('div');
-    pantalla.classList.add('seccionArticuloUnitario')
-    pantalla.innerHTML = `
-        <h2>${productos.nombre}</h2>
-        <img src="${productos.img}" alt="">
-        <p>${productos.precio}</p>
-        <input type="button" id="añadir" value="Añadir al Carrito">
-    `
-    div_lipieza.appendChild(pantalla);
+seccion_limpieza.forEach((productos, indice) => {
+    div_lipieza.innerHTML += `<div  class="seccionArticuloUnitario" id="producto${indice}">
+                            <h2>${productos.nombre}</h2>
+                            <img src="${productos.img}" alt="">
+                            <p>${productos.precio}</p>
+                            <input type="button" id="añadir" value="Añadir al Carrito">
+                        </div>`
 })
 
 const div_bebidas = document.getElementById('seccion_bebidas')
 
-seccion_bebida.forEach(productos => {
-    const pantalla = document.createElement('div');
-    pantalla.classList.add('seccionArticuloUnitario')
-    pantalla.innerHTML = `
-        <h2>${productos.nombre}</h2>
-        <img src="${productos.img}" alt="">
-        <p>${productos.precio}</p>
-        <input type="button" id="añadir" value="Añadir al Carrito">
-    `
-    div_bebidas.appendChild(pantalla);
+seccion_bebida.forEach((productos,indice) => {
+    div_bebidas.innerHTML += `<div  class="seccionArticuloUnitario" id="producto${indice}">
+                            <h2>${productos.nombre}</h2>
+                            <img src="${productos.img}" alt="">
+                            <p>${productos.precio}</p>
+                            <input type="button" id="añadir" value="Añadir al Carrito">
+                        </div>`
 })
 
 const div_bazar = document.getElementById('seccion_bazar')
 
-seccion_bazar.forEach(productos => {
-    const pantalla = document.createElement('div');
-    pantalla.classList.add('seccionArticuloUnitario')
-    pantalla.innerHTML = `
-        <h2>${productos.nombre}</h2>
-        <img src="${productos.img}" alt="">
-        <p>${productos.precio}</p>
-        <input type="button" id="añadir" value="Añadir al Carrito">
-    `
-    div_bazar.appendChild(pantalla);
+seccion_bazar.forEach((productos, indice) => {
+    div_bazar.innerHTML += `<div  class="seccionArticuloUnitario" id="producto${indice}">
+                            <h2>${productos.nombre}</h2>
+                            <img src="${productos.img}" alt="">
+                            <p>${productos.precio}</p>
+                            <input type="button" id="añadir" value="Añadir al Carrito">
+                        </div>`
 })
 
 const div_perfumeria = document.getElementById('seccion_perfumeria')
 
-seccion_perfumeria.forEach(productos => {
-    const pantalla = document.createElement('div');
-    pantalla.classList.add('seccionArticuloUnitario')
-    pantalla.innerHTML = `
-        <h2>${productos.nombre}</h2>
-        <img src="${productos.img}" alt="">
-        <p>${productos.precio}</p>
-        <input type="button" id="añadir" value="Añadir al Carrito">
-    `
-    div_perfumeria.appendChild(pantalla);
+seccion_perfumeria.forEach((productos, indice) => {
+    div_perfumeria.innerHTML += `<div  class="seccionArticuloUnitario" id="producto${indice}">
+                            <h2>${productos.nombre}</h2>
+                            <img src="${productos.img}" alt="">
+                            <p>${productos.precio}</p>
+                            <input type="button" id="añadir" value="Añadir al Carrito">
+                        </div>`
 })
 //--------------------------------------------------------------------------------------------------------------
-// const btn_añadir = document.getElementById('añadir')
 
-// const seccion_carrito = document.getElementById('seccion_carrito')
-
-// btn_añadir.addEventListener('click', ()=> {
-//     seccion_almacen.forEach((producto) => {
-//         seccion_carrito.innerHTML +=   `<div>
-//                                             <tr>
-//                                                 <td><b>Producto</b></td>
-//                                                 <td><b>Cantidad</b></td>
-//                                                 <td><b>Precio</b></td>
-//                                                 <td><b>Sub Total</b></td>
-//                                             </tr>>
-//                                         </div>`
-//     })
-// })
 
